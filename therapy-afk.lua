eba2a2d0dbfcccea8c85a843e74b2181676f28c7
lithium <lithetanium@gmail.com>
2025-10-28 15:13:51 +0100

    Add AFK script loading on teleport

--- therapy-afk.lua
+++ therapy-afk.lua
@@ -6,7 +6,7 @@ local pid=game.PlaceId
 local jid=game.JobId
 
 if queue_on_teleport then
-    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/lithium1on/hub/refs/heads/main/therapy.lua"))()')
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/lithium1on/hub/refs/heads/main/therapy-afk.lua"))()')
 end
 
 local total=0
