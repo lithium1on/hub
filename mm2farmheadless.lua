a78d8170343d758e8f08c100f873d736b8c5011b
lithium <lithetanium@gmail.com>
2026-04-24 22:59:07 +0200

    i commit

--- mm2farmheadless.lua
+++ mm2farmheadless.lua
@@ -236,7 +236,7 @@ function AutoFarm:StartFarmingLoop()
         while true do
             task.wait()
 
-            if Settings.Rendering ~= game:GetService("RunService"):Is3dRenderingEnabled() then
+            if Settings.Rendering ~= game:GetService("RunService"):Set3dRenderingEnabled() then
                 game:GetService("RunService"):Set3dRenderingEnabled(Settings.Rendering)
             end
 
